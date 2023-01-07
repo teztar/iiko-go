2a1db5cd4bcf14f9a8005bb09a7773c6c427fad7
Shamil-R <havk13@gmail.com>
2023-01-07 10:46:16 +0700

    fix WalletBalance fields type

--- wallet_balance.go
+++ wallet_balance.go
@@ -1,8 +1,18 @@
 package iiko
 
+type WalletType int
+
+const (
+	WalletTypeDepositOrCorporateNutrition WalletType = 0
+	WalletTypeBonusProgram                WalletType = 1
+	WalletTypeProductsProgram             WalletType = 2
+	WalletTypeDiscountProgram             WalletType = 3
+	WalletTypeCertificateProgram          WalletType = 4
+)
+
 type WalletBalance struct {
-	Id      string `json:"id"`
-	Name    string `json:"name"`
-	Type    int    `json:"type"`
-	Balance int    `json:"balance"`
+	Id      string     `json:"id"`
+	Name    string     `json:"name"`
+	Type    WalletType `json:"type"`
+	Balance float64    `json:"balance"`
 }
