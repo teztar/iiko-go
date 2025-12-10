cfc5984a7c0917c3c4e8e73586a0d0f199c2a6b2
Ron <ib.komron@gmail.com>
2025-12-10 12:12:04 +0500

    chore(make_request): Temporary measure of changing the visibility zone of the post request method

--- make_request.go
+++ make_request.go
@@ -63,3 +63,56 @@ func (c *Client) post(requiresAuth bool, endpoint string, body interface{}, resp
 
 	return nil
 }
+
+func (c *Client) Post(requiresAuth bool, endpoint string, body interface{}, response interface{}, opts ...Option) error {
+	if requiresAuth && c.token == "" {
+		return ErrMissingToken
+	}
+
+	// Marshal json body for request.
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	// Create request.
+	req, err := http.NewRequest(http.MethodPost, c.baseURL+endpoint, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return err
+	}
+
+	// Set headers.
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	if requiresAuth {
+		req.Header.Set("Authorization", "Bearer "+c.token)
+	}
+	req.Header.Set("Timeout", strconv.Itoa(int(c.timeout.Seconds())))
+
+	// Apply custom options to request.
+	for _, opt := range opts {
+		opt.Apply(req)
+	}
+
+	// Make request.
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		var errorResponse ErrorResponse
+		if err = json.NewDecoder(resp.Body).Decode(&errorResponse); err != nil {
+			return err
+		}
+		errorResponse.StatusCode = resp.StatusCode
+		return &errorResponse
+	}
+
+	// If success.
+	if err = json.NewDecoder(resp.Body).Decode(response); err != nil {
+		return err
+	}
+
+	return nil
+}
